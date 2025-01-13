4607d2866a8875b12d856c960a0d31961dafa1d8
Lloyd <gwapolloyd124@gmail.com>
2025-01-14 00:26:45 +0800

    fix bug

--- components/Notice.tsx
+++ components/Notice.tsx
@@ -5,7 +5,7 @@ export default function Notice() {
     <div className="bg-blue-100 border border-blue-300 text-blue-900 rounded-md p-4 flex items-center space-x-4 shadow-sm">
       <Wrench className="w-6 h-6 text-blue-900" />
       <div>
-        <h3 className="text-lg font-semibold">We're Still Building!</h3>
+        <h3 className="text-lg font-semibold">We&apos;re Still Building!</h3>
         <p>
           This area is under development as we strive to create the best
           experience for you. Thank you for your patience and support!
